5d6eaec1fb456af48fa55c1473f12b68cc62d314
seimori <issei.morimoto@gmail.com>
2020-09-14 06:16:23 +0200

    Trying to clean node_modules

---
+++ whatsapp-clone-client/src/App.tsx
@@ -0,0 +1,10 @@
+import React, { cloneElement } from 'react';
+import ChatsListScreen from './components/ChatsListScreen';
+
+const App: React.FC = () => (
+  <div>
+    <ChatsListScreen />
+  </div>
+);
+
+export default App;
