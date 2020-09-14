9bd4e431fb56ab55a3442178ed51da78742cbf67
seimori <issei.morimoto@gmail.com>
2020-09-14 06:19:17 +0200

    Add missing server files

---
+++ whatsapp-clone-server/index.ts
@@ -0,0 +1,22 @@
+import { ApolloServer, gql } from 'apollo-server-express';
+import cors from 'cors';
+import express from 'express';
+import schema from './schema';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const server = new ApolloServer({ schema });
+
+server.applyMiddleware({
+  app,
+  path: '/graphql',
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
